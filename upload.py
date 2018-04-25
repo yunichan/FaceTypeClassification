80fd3bcf22a50613ecf4d91682240bb1e153d841
yuni <yuni.tonoseaki@gmial.com>
2018-04-25 12:29:29 +0900

    add

---
+++ upload.py
@@ -0,0 +1,78 @@
+import os
+import sqlite3
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session
+from werkzeug import secure_filename
+import eval
+import tensorflow as tf
+import multiprocessing as mp
+import numpy as np
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'gif'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = os.urandom(24)
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def index():
+    """HTMLページをレンダリングします."""
+    return render_template('index.html')
+
+# @app.route('/')
+# def index():
+#     if 'username' in session:
+#         return render_template('index.html')
+#     return '''
+#         <p>ログインしてください</p>
+#     '''
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         username = request.form['username']
+#         if username == 'admin':
+#             session['username'] = request.form['username']
+#             return redirect(url_for('index'))
+#         else:
+#             return '''<p>ユーザー名が違います</p>'''
+#     return '''
+#         <form action="" method="post">
+#             <p><input type="text" name="username">
+#             <p><input type="submit" value="Login">
+#         </form>
+#     '''
+
+# @app.route('/logout')
+# def logout():
+#     session.pop('username', None)
+#     return redirect(url_for('index'))
+
+@app.route('/send', methods=['GET', 'POST'])
+def send():
+    if request.method == 'POST':
+        img_file = request.files['img_file']
+        if img_file and allowed_file(img_file.filename):
+            filename = secure_filename(img_file.filename)
+            img_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            img_file.save(img_path)
+            img_url = '/uploads/' + filename
+            result = eval.evaluation(img_path, '/Users/yuni/facetype_app/model.ckpt')
+            return render_template('index.html', result=result)
+            
+        else:
+            return ''' <p>許可されていない拡張子です</p> '''
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
